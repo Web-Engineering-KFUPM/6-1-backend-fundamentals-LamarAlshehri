6e46c29f9b93ba6401ce0315cd9a888dba0f1291
LamarAlshehri <lamar.a.alshehri@gmail.com>
2026-03-30 13:05:36 +0300

    todo5

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -10,8 +10,9 @@ import cors from 'cors';
 app.use(cors());
 
 // TODO 5: Create the home route "/"
-
-
+app.get('/', (req, res) => {
+  res.send('Hello from the back-end server');
+});
 
 // TODO 6: Create the "/about" route
 
